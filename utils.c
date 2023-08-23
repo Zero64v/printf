355fb36568f425a08a1b9acb1b9a444325bb66b8
Zero64v <kijanavictor96@gmail.com>
2023-08-23 11:23:43 -0700

    print

--- utils.c
+++ utils.c
@@ -50,10 +50,15 @@ return (0);
 * Return: Casted value of num
 */
 long int convert_size_number(long int num, int size)
+{
 if (size == S_LONG)
+{
 return (num);
+}
 else if (size == S_SHORT)
+{
 return ((short)num);
+}
 return ((int)num);
 }
 /**
@@ -66,8 +71,12 @@ return ((int)num);
 long int convert_size_unsgnd(unsigned long int num, int size)
 {
 if (size == S_LONG)
+{
 return (num);
+}
 else if (size == S_SHORT)
+{
 return ((unsigned short)num);
+}
 return ((unsigned int)num);
 }
